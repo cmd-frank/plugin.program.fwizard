4e41a694e62ccfa3e062eaa06e4b0fb45671179b
SEA6ULL <epicaltfirematt382@gmail.com>
2024-11-18 15:01:16 -0800

    added all wizard files

---
+++ uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/SEA6ULL/Wizard/master/builds.json'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'http://raw.githubusercontent.com/SEA6ULL/Wizard/master/notify.txt'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
